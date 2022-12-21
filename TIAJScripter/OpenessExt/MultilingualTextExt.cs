b4eeeb6636e8e6c5c85ca5a7f06ed1c79249ad45
Simon Berg <ksb@fluffware.se>
2022-12-21 08:35:09 +0000

    Missing file.

---
+++ TIAJScripter/OpenessExt/MultilingualTextExt.cs
@@ -0,0 +1,27 @@
+﻿using Siemens.Engineering;
+using System;
+using System.Globalization;
+
+namespace TIAJScripter.OpenessExt
+{
+    public static class MultilingualTextExt
+    {
+        public static Project Project = null;
+        public static string GetText(this MultilingualText self, string culture = null)
+        {
+            if (Project == null) throw new Exception("No project active");
+            var lang = Project.LanguageSettings.Languages.Find(new CultureInfo(culture));
+            var item = self.Items.Find(lang);
+            return (item != null) ? item.Text : "<No text>";
+        }
+
+        public static void SetText(this MultilingualText self, string culture, string value)
+        {
+            if (Project == null) throw new Exception("No project active");
+            var lang = Project.LanguageSettings.Languages.Find(new CultureInfo(culture));
+            var item = self.Items.Find(lang);
+            if (item == null) return;
+            self.Items.Find(lang).Text = value;
+        }
+    }
+}
